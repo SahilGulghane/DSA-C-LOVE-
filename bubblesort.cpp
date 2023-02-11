08a7f14e799bf59bea388ab87c06313a6fb4d43c
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2023-02-11 20:12:15 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,6 +2,8 @@
 // If you want, you can also implement bubble sort to sort the values in descending order.
 // A real-world example of a bubble sort algorithm is how the contact list on your phone is sorted in alphabetical order
 
+//it is stable algorithim
+
 
 #include<vector>
 using namespace std;
