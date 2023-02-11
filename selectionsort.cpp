8db93b55c24ea8d64be14a795a9d119001619ef1
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2023-02-11 19:50:57 +0530

    Create selectionsort.cpp

---
+++ selectionsort.cpp
@@ -0,0 +1,26 @@
+
+
+// Selection sort is an effective and efficient sort algorithm based on comparison operations. It adds one element in each iteration. 
+// You need to select the smallest element in the array and move it to the beginning of the array by swapping with the front element
+
+
+
+#include<vector>
+using namespace std;
+void selectionSort(vector<int>& arr, int n)
+{  
+   for(int i =0 ; i< n-1 ; i++){
+
+       int min = i;
+
+       for(int j =i+1 ; j<n ;j++){
+
+           if(arr[j] < arr[min]){
+               min = j;
+               
+           }
+       }
+       swap(arr[min] , arr[i]);
+   }
+
+}
